3c057f80bc237f7be521d1ec9058d367a8bd088f
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-15 15:02:43 +0530

    Added: Oops problem

---
+++ src/AccountManaagement/ManageAccount.java
@@ -0,0 +1,40 @@
+package AccountManaagement;
+
+import java.util.Scanner;
+
+public class ManageAccount {
+	
+	Scanner scanner = new Scanner(System.in); // create object
+
+	static String name;
+	private static int balance = 10000;
+
+	public void viewBalance() {
+		System.out.println(balance);
+
+	}
+
+	public void addBalance() {
+		System.out.println("Please enter the amount to add: ");
+		int amount = scanner.nextInt();
+		balance = balance + amount; 
+
+		System.out.println("The updated account balance is: " + balance);
+	}
+
+	public void withdrawBalance() {
+		System.out.println("Enter the amount to be withdraw: ");
+		int amount = scanner.nextInt();
+		if (amount <= 0) {
+			System.out.println("Enter valid amount for withdraw: ");
+
+		} else if (amount <= balance) {
+			System.out.println("Withdraw is under process \nWithdraw process is successfull..");
+			balance = balance - amount; 
+			System.out.println("Updated balance is = " + balance);
+		} else {
+			System.out.println("Insufficient balance...");
+		}
+	}
+	
+}
